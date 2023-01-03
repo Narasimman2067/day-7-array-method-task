b81253401eaa501084da698c62222c79e1667f6d
Narasimman2067 <narasimmamech2067@gmail.com>
2023-01-04 01:31:49 +0530

    day 7 task

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Get all the countries from the Asia continent /region using the Filter function
+// // Get all the countries from the Asia continent /region using the Filter function
 
 
 var request = new XMLHttpRequest();
@@ -14,7 +14,7 @@ request.onload =function()
 }
 
 
-// Get all the countries with a population of less than 2 lakhs using Filter function  
+// // Get all the countries with a population of less than 2 lakhs using Filter function  
    
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true);
@@ -30,7 +30,7 @@ request.onload =function()
 
 
 
-// Print the following details name, capital, flag using forEach function
+// // Print the following details name, capital, flag using forEach function
 
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true);
@@ -46,3 +46,52 @@ request.onload =function()
         `)
     });
 }
+
+
+
+// Print the total population of countries using reduce function
+
+
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all",true);
+request.send();
+request.onload =function()
+{
+    var data =request.response;
+    var result3 =JSON.parse (data) 
+    var res3=result3.reduce((acc,cv)=>acc+cv.population,0)
+    console.log(res3);
+}
+
+
+
+
+//Print the country which uses US Dollars as currency.
+
+
+
+var request =new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all",true)
+request.send()
+request.onload=function(){
+    var data=request.response
+    var result=JSON.parse(data)
+    var res=result.filter((ele)=>{
+        for(let key in ele.currencies){
+            if(ele.currencies[key].code==="USD")
+        {
+            console.log(ele.name)
+        }
+    }
+       
+    })
+}
+
+
+
+
+    
+   
+       
+    
+
